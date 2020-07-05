945e8e6a157bd126981cd11a639f0fa947a43fdf
unknown <npriyajit1@gmail.com>
2020-07-05 11:18:38 +0530

    Complete rocket loader

---
+++ app.js
@@ -0,0 +1,24 @@
+function main() {
+  const scene = document.querySelector(".scene");
+  const count = 50;
+  for (let i = 0; i < count; i++) {
+    const left = Math.floor(Math.random() * innerWidth);
+    const duration = Math.random() * 1 + 0.2;
+    const height = Math.random() * 100;
+    let star = document.createElement("i");
+    star.style.left = left + "px";
+    star.style.width = 1 + "px";
+    star.style.height = height + "px";
+    star.style.animationDuration = duration + "s";
+    scene.appendChild(star);
+  }
+}
+
+function drag() {
+  const range = document.querySelector("#range");
+  document.querySelectorAll("i").forEach((star) => {
+    star.style.height = range.value + "px";
+  });
+}
+
+window.addEventListener("load", main);
